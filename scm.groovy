7638949b2327b18ae185bad0a4ba644f9b3c6d8a
Chris <chris.kok@gmail.com>
2018-02-02 13:41:47 +0800

    Updated

--- scm.groovy
+++ scm.groovy
@@ -5,7 +5,7 @@ def checkoutGitSourceCode (credentialsId_arg, url_arg, branch_arg) {
     doGenerateSubmoduleConfiguration: false,
     extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: true]],
     gitTool: 'GIT',
-    userRemoteConfigs: [[credentialsId_arg, url_arg, branch_arg]]
+    userRemoteConfigs: [[credentialsId: credentialsId_arg, url: url_arg]]
     ]
   )
 }
