e9060d6f9a32b2899d8e58f23590cba25e7b50f0
Chris <chris.kok@gmail.com>
2018-02-02 14:48:53 +0800

    Updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,15 +9,6 @@ node {
       scm = load('scm.groovy')
       mvn = load('mvn.groovy')
    }
-   stage('Version') {
-      def mvnHome = tool 'M3'
-      // Run the maven build
-      if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -version"
-      } else {
-         bat(/"${mvnHome}\bin\mvn" -version/)
-      }
-   }
    stage('checkout') {
     scm.checkoutGitSourceCode('	026422e2-e06c-46ca-aaaf-027bc255ed70', 'https://github.com/chriscikok/temp.git', '*/master')
    }
--- mvn.groovy
+++ mvn.groovy
@@ -2,7 +2,19 @@ def build(project_folder_arg) {
   def mvnHome = tool 'M3'
   def curPath = pwd()
   dir("${curPath}/${project_folder_arg}")
-  def exist = fileExists 'pom.xml'
-  echo exist
+  if (fileExists('pom.xml')) {
+      echo 'Yes'
+  } else {
+      echo 'No'
+  }
 }
-return this
+
+def version() {
+  def mvnHome = tool 'M3'
+  // Run the maven build
+  if (isUnix()) {
+     sh "'${mvnHome}/bin/mvn' -version"
+  } else {
+     bat(/"${mvnHome}\bin\mvn" -version/)
+  }
+}
