a59fb859be982861b26107e7e6ee02531ac17505
Chris <chris.kok@gmail.com>
2018-02-02 15:12:35 +0800

    Updated

--- mvn.groovy
+++ mvn.groovy
@@ -1,7 +1,7 @@
 def build(project_folder_arg) {
   def mvnHome = tool 'M3'
   def curPath = pwd()
-  dir(pwd()+'/'+${project_folder_arg})
+  dir("${curPath}/${project_folder_arg}")
   if (fileExists('pom.xml')) {
       echo 'Yes'
   } else {
