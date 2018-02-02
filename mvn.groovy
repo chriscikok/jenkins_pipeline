ae0c50a0426ad0c71a2749016d8cb43bc229ef1b
Chris <chris.kok@gmail.com>
2018-02-02 13:59:24 +0800

    Updated

--- mvn.groovy
+++ mvn.groovy
@@ -1,8 +1,6 @@
 def build(project_folder_arg) {
   def mvnHome = tool 'M3'
-  stage ('build') {
-    sh "'cd ' project_folder_arg"
-    sh "ls -ls"
-  }
+  sh "'cd ' project_folder_arg"
+  sh "ls -ls"
 }
 return this
